c37804546921efefc73b1b592d52b161bb820af5
Bhargav Vanra <88825575+Bhargav007SGL@users.noreply.github.com>
2022-07-21 12:22:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.get("/api/whoami", function (req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Your app is listening on http://${ip.address()}:${listener.address().port}`)
+})
